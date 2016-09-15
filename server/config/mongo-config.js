5a8da4ae9006629ee17a03a9db301848eb35680f
mnichangxin <mnichangxin@vip.qq.com>
2016-09-16 01:05:30 +0800

    modify mongo

--- server/config/mongo-config.js
+++ server/config/mongo-config.js
@@ -2,5 +2,7 @@ module.exports = {
     PORT: 27017,
     HOST: '10.10.72.139',
     // HOST: 'localhost',
+    USERNAME: 'uuH4Qidf3BomjEnx',
+    PASSWORD: 'pagRYxTpD16LeqJmZ',
     NAME: 'iE8zr6dubwtGsBOQ'
 }
--- server/models/db-mongo.js
+++ server/models/db-mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../config/mongo-config');
 
-const db = mongoose.connect("mongodb://" + config.HOST + ":" + config.PORT + "/" + config.NAME);
+const db = mongoose.connect("mongodb://" + config.USERNAME + ":" +  config.PASSWORD + "@" + config.HOST + ":" + config.PORT + "/" + config.NAME);
 
 module.exports = db;
